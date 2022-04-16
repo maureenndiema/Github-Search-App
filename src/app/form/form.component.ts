a0638a9e2f362b34f5aa00b0b9eb936345bbdf03
maureenndiema <ndiemam@gmail.com>
2022-04-16 15:33:14 +0300

    add loaders

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -13,3 +13,4 @@ export class FormComponent implements OnInit {
   
   ngOnInit(): void {
   }
+}
--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./github.component.css']
 })
 export class GithubComponent implements OnInit {
+  this.http.get<ApiResponse>("http://quotes.stormconsultancy.co.uk/random.json").subscribe(data=>{
+      // Succesful API request
+      this.quote = new Quote(data.author, data.quote)
+    },err=>{
+        this.quote = new Quote("Winston Churchill","Never never give up!")
+        console.log("An error occurred")
+    })
+  
 
   constructor() { }
 
