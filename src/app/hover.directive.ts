f869750c7db9f8ef09bbd31a33cdb5e8a6a08c43
maureenndiema <ndiemam@gmail.com>
2022-04-17 18:38:39 +0300

    add hover directive

--- src/app/hover.directive.ts
+++ src/app/hover.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHover]'
@@ -8,5 +8,16 @@ export class HoverDirective {
   constructor(private element: ElementRef, private renderer: Renderer2) { 
 
   }
-
+  @HostListener('mouseenter')onemouseover (){
+    this.renderer.setStyle(this.element.nativeElement, 'margin','5px 10px');
+    this.renderer.setStyle(this.element.nativeElement, 'padding','30px 30px');
+    this.renderer.setStyle(this.element.nativeElement, 'transition','0.5s');
+    
+  }
+  @HostListener('mouseleave')onemouseout (){
+    this.renderer.setStyle(this.element.nativeElement, 'margin','10px 20px');
+    this.renderer.setStyle(this.element.nativeElement, 'padding','10px 20px');
+    this.renderer.setStyle(this.element.nativeElement, 'transition','0.5s');
+    
+  }
 }
