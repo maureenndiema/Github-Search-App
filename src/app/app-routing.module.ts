24fb89f3de7e0e9278458447ff915f493193a47a
maureenndiema <ndiemam@gmail.com>
2022-04-17 18:13:27 +0300

    add changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GithubComponent } from './github/github.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
 
 const routes: Routes = [
   { path: 'github', component: GithubComponent},
+  { path:'**', component:NotFoundComponent},
+
+  { path: '', redirectTo:"/goals", pathMatch:"full"},
 ];
 
 @NgModule({
