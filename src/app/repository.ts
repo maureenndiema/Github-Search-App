5589d5d47ef553011bd6a3c7aada21488025e00f
maureenndiema <ndiemam@gmail.com>
2022-04-18 20:07:56 +0300

    add pending changes

--- src/app/repository.ts
+++ src/app/repository.ts
@@ -1,2 +1,15 @@
 export class Repository {
-}
+   
+    constructor(
+        public username:string,
+        public html_url:string ,
+        public description:string,
+        public forks:number,
+        public language:string,
+        public created_at:Date,
+        public watchers_count:number,
+        public homepage: string,){
+      }
+  }
+
+
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,4 +1,19 @@
 export class User {
-    constructor(private username:string,){}
+    constructor(
+            public username:string,
+            public login:string, 
+            public html_url:string,
+            public bio: string,
+            public location:string, 
+            public public_repos:number,
+            public followers:number,
+            public following:number,
+            public avatar_url:string,
+            public created_at:Date){
+       
+         }
+        
 
-}
+    }
+
+
